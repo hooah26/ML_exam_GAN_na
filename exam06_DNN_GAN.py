3bed1003ad7e3103e0b3b1af0c0f9ffd96642b28
hooah26 <hooah26@gmail.com>
2022-04-18 14:36:27 +0900

    first

---
+++ exam06_DNN_GAN.py
@@ -0,0 +1,84 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import Sequential
+
+OUT_DIR = './DNN_out'  # 저장 폴더 지정
+img_shape = (28, 28, 1)  # 이미지 모양
+epochs = 100000
+batch_size = 128
+noise = 100
+sample_interval = 100
+
+(X_train, _), (_, _) = mnist.load_data()  #mnist 트레인 파일 가져옴
+print(X_train.shape)
+
+X_train = X_train / 127.5 - 1 # 범위를 -1 ~ 1사이로 지정 , 옅은 색은 학습이 덜 되게 해라
+X_train = np.expand_dims(X_train, axis=3)
+print(X_train.shape)
+
+generator = Sequential() #generator 모델 -값을 조금 살리려고 tanh activation 사용
+generator.add(Dense(128, input_dim=noise))
+generator.add(LeakyReLU(alpha=0.01)) # -값을 약하게 1/100으로 약하게 살리려고
+generator.add(Dense(784, activation='tanh')) # tanh 을 쓰면 마이너스값을 살리려고 씀
+generator.add(Reshape(img_shape))
+generator.summary()
+
+lrelu = LeakyReLU(alpha=0.01)
+discriminator = Sequential()
+discriminator.add(Flatten(input_shape=img_shape))
+discriminator.add(Dense(128, activation=lrelu))
+discriminator.add(Dense(1, activation='sigmoid')) #2진분류
+discriminator.summary()
+
+discriminator.compile(loss='binary_crossentropy',
+                      optimizer='adam', metrics=['accuracy'])
+discriminator.trainable=False
+
+
+gan_model = Sequential()
+gan_model.add(generator)
+gan_model.add(discriminator)
+gan_model.summary()
+gan_model.compile(loss='binary_crossentropy', optimizer='adam')
+
+real = np.ones((batch_size, 1))
+
+fake = np.zeros((batch_size, 1))
+
+for epoch in range(epochs):
+    idx = np.random.randint(0, X_train.shape[0], batch_size)
+    real_imgs = X_train[idx]
+
+    z = np.random.normal(0, 1, (batch_size, noise))
+    fake_imgs = generator.predict(z)
+
+    d_hist_real = discriminator.train_on_batch(real_imgs, real)
+    d_hist_fake = discriminator.train_on_batch(fake_imgs, fake)
+
+    d_loss, d_acc = 0.5 * np.add(d_hist_real, d_hist_fake)
+    discriminator.trainable=False
+    if epoch % 2 == 0:
+        z = np.random.normal(0, 1, (batch_size, noise))
+        gan_hist = gan_model.train_on_batch(z, real)
+
+    if epoch % sample_interval == 0:
+        print('%d [D loss: %f, acc.: %.2f%%] [G loss: %f]'%(
+            epoch, d_loss, d_acc * 100, gan_hist))
+        row = col = 4
+        z = np.random.normal(0, 1, (row * col, noise))
+        fake_imgs = generator.predict(z)
+        fake_imgs = 0.5 * fake_imgs + 0.5
+        _, axs = plt.subplots(row, col, figsize=(row, col),
+                             sharey=True, sharex=True)
+        cont = 0
+        for i in range(row):
+            for j in range(col):
+                axs[i, j].imshow(fake_imgs[cont, :, :, 0], cmap='gray')
+                axs[i, j].axis('off')
+                cont += 1
+        path = os.path.join(OUT_DIR, 'img-{}'.format(epoch+1))
+        plt.savefig(path)
+        plt.close()
